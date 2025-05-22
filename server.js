4cfb4df3a51c9590fd1900a64e433845ba00333f
LiShaojun1126 <yur1loverslave@gmail.com>
2025-05-22 19:16:12 -0400

    finished all tasks

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get("/", (req, res) => {
 // GET all products
 app.get("/products", async (req, res) => {
   // TODO: Query database and return all products
+  try {
+    //await pool.query("SELECT * FROM products");
+    res.json(await pool.query("SELECT * FROM products"));
+  } catch (err) {
+    res.status(500).json({ status: "error", database: "disconnected" });
+  }
 });
 
 // GET single product
@@ -44,6 +50,18 @@ app.get("/products/:id", async (req, res) => {
   // TODO: 1. Get ID from params
   //       2. Query database
   //       3. Handle not found case
+  try{
+    const id = req.params.id;
+    const result = await pool.query("SELECT * FROM products WHERE id = $1", [id]);
+    //handle not found case
+    if(result.rows[0] === 0){
+      return res.status(404).json({status: "error", database: "Product not found"});
+    }
+
+    res.json(result.rows[0]);
+  } catch(err){
+    res.status(500).json({status: "error", database: "disconnected"});
+  }
 });
 
 // POST create product
@@ -51,6 +69,20 @@ app.post("/products", async (req, res) => {
   // TODO: 1. Validate required fields (name, price)
   //       2. Insert into database
   //       3. Return new product
+  try{
+    const {name, price} = req.body;
+    if(!name || price === undefined){
+      return res.status(400).json({error: "missing name and price"});
+    }
+    const result = await pool.query("INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *",
+      [name, price]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch(err){
+    res.status(500).json({status: "error", database: "disconnected"});
+  }
+
 });
 
 // PUT update product
@@ -58,12 +90,44 @@ app.put("/products/:id", async (req, res) => {
   // TODO: 1. Get ID from params
   //       2. Validate inputs
   //       3. Update database
+    
+    try{
+    const {id} = req.params;
+    const {name, price} = req.body;
+
+    if(name !== undefined || price !== undefined){
+      return res.status(400).json({error: "Invalid data"});
+    }
+
+    const result = await pool.query("UPDATE products SET name = $1, price = $2 WHERE id = $3 RETURNING *",[name, price, id]);
+
+    res.json(result.rows[0]);
+
+      
+    } catch(err){
+      res.status(500).json({status: "error", database: "disconnected"});
+    }
+
 });
 
 // DELETE product
 app.delete("/products/:id", async (req, res) => {
   // TODO: 1. Delete from database
   //       2. Handle success/failure
+  try{
+    const {id} = req.params.id;
+    
+    const result = await pool.query("DELETE FROM products WHERE id = $1", [id]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json({ status: "success", message: "Product deleted" });
+
+      
+    } catch(err){
+      res.status(500).json({status: "error", database: "disconnected"});
+    }
 });
 
 // ------------------------------------------------
